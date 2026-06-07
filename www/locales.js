c77dc4f82c419b47249958b09c8f76a308dc5d97
devdly3 <gloomky.taylor@gmail.com>
2026-06-07 15:57:01 -0400

    agregar login registro y sesion de usuario

---
+++ www/locales.js
@@ -0,0 +1,7 @@
+const locales = [
+{ nombre: "Pronto Copec", mall: "La Reina", piso: "", nota: "Entrada por Monseñor Edwards, al fondo a la derecha, no entrar a la tienda", emoji: "", lat: -33.438222, lng: -70.561222 },
+{ nombre: "Papa John's", mall: "Cerrillos", piso: "", nota: "Entrada por Pedro Aguirre Cerda", emoji: "", lat: -33.501333, lng: -70.710806 },
+{ nombre: "Cevichitos", mall: "Santiago Centro", piso: "", nota: "Frente a la plaza Brasil", emoji: "", lat: -33.440056, lng: -70.666167 },
+{ nombre: "Papa John's", mall: "Renca", piso: "", nota: "Entrada por Av. Dorsal", emoji: "", lat: -33.405611, lng: -70.704444 },
+{ nombre: "Dgusta", mall: "Renca", piso: "", nota: "Por la ciclovía", emoji: "", lat: -33.403528, lng: -70.703750 },
+];
